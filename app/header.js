72e43154881e9bf06dd256ec16cebb5f3ac80756
James Donald <dev@chaoscantrip.com>
2023-04-13 07:11:10 +0100

    Added header shadow and wrapper

--- app/header.js
+++ app/header.js
@@ -2,19 +2,21 @@ import styles from "@/styles/modules/header.module.css";
 
 export default function Header(){
     return (
-        <header className={styles.header}>
-            <div className={styles.title}>
-                <h1>Happy Trails Waggy Tails</h1>
-                <p>Website currently under development :)</p>
+        <header className={styles.wrapper}>
+            <div className={styles.content}>
+                <div className={styles.title}>
+                    <h1>Happy Trails Waggy Tails</h1>
+                    <p>Website currently under development :)</p>
+                </div>
+                <nav className={styles.nav}>
+                    <ul>
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/about">About</a></li>
+                        <li><a href="/services">Services</a></li>
+                        <li><a href="/contact">Contact</a></li>
+                    </ul>
+                </nav>
             </div>
-            <nav className={styles.nav}>
-                <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/about">About</a></li>
-                    <li><a href="/services">Services</a></li>
-                    <li><a href="/contact">Contact</a></li>
-                </ul>
-            </nav>
         </header>
     )
 }
