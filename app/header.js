d656bc717816f64393ae42946d3ac07178511dc2
James Donald <dev@chaoscantrip.com>
2023-04-13 07:37:31 +0100

    Changed nav to Link elements

--- app/header.js
+++ app/header.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 import styles from "@/styles/modules/header.module.css";
 
 export default function Header(){
@@ -10,10 +12,10 @@ export default function Header(){
                 </div>
                 <nav className={styles.nav}>
                     <ul>
-                        <li><a href="/">Home</a></li>
-                        <li><a href="/about">About</a></li>
-                        <li><a href="/services">Services</a></li>
-                        <li><a href="/contact">Contact</a></li>
+                        <Link href="home"><li>Home</li></Link>
+                        <Link href="about"><li>About</li></Link>
+                        <Link href="services"><li>Services</li></Link>
+                        <Link href="contact"><li>Contact</li></Link>
                     </ul>
                 </nav>
             </div>
