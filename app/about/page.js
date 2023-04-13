6a070866795489e5a4a20d334bcf7dbba918d980
James Donald <dev@chaoscantrip.com>
2023-04-13 05:09:52 +0100

    Added content to push footer down

--- app/about/page.js
+++ app/about/page.js
@@ -5,6 +5,26 @@ export default function AboutPage() {
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A amet, architecto aspernatur atque blanditiis
                 dicta doloribus earum excepturi exercitationem labore maxime omnis quisquam, quod sint soluta sunt ullam
                 velit voluptatum.</p>
+            <p>Content</p>
+            <p>To</p>
+            <p>Push</p>
+            <p>Footer</p>
+            <p>Down</p>
+            <p>Content</p>
+            <p>To</p>
+            <p>Push</p>
+            <p>Footer</p>
+            <p>Down</p>
+            <p>Content</p>
+            <p>To</p>
+            <p>Push</p>
+            <p>Footer</p>
+            <p>Down</p>
+            <p>Content</p>
+            <p>To</p>
+            <p>Push</p>
+            <p>Footer</p>
+            <p>Down</p>
         </div>
     )
 }
