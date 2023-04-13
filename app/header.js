7a7a65397c92ee4cdbcafecfc2afc93c898d5c17
James Donald <dev@chaoscantrip.com>
2023-04-13 04:37:38 +0100

    Added nav to header

--- app/header.js
+++ app/header.js
@@ -3,8 +3,18 @@ import styles from "@/styles/modules/header.module.css";
 export default function Header(){
     return (
         <header className={styles.header}>
-            <h1>Happy Trails Waggy Tails</h1>
-            <p>Website currently under development :)</p>
+            <div className={styles.title}>
+                <h1>Happy Trails Waggy Tails</h1>
+                <p>Website currently under development :)</p>
+            </div>
+            <nav className={styles.nav}>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/about">About</a></li>
+                    <li><a href="/services">Services</a></li>
+                    <li><a href="/contact">Contact</a></li>
+                </ul>
+            </nav>
         </header>
     )
 }
