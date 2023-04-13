2f8aa503adcfb8e0b89903bb91a5b7d10b7eff22
James Donald <dev@chaoscantrip.com>
2023-04-13 04:00:30 +0100

    Added initial page content

--- app/home/page.js
+++ app/home/page.js
@@ -5,7 +5,16 @@ export const metadata = {
 export default function HomePage() {
   return (
     <div>
-      <h1>Home Page</h1>
+        <p>Hi, I'm Nadine</p>
+        <p>After working for most of my adult life, specifically the last 9 years, in a school for children with Special Education Needs, I am not following a long held ambition to work with dogs, and have set up Happy Trails Waggy Tails dog walking service</p>
+        <p>I would love to hear from you and meet your doggy member of the family</p>
+        <ul>
+            <li>Solo or group walks</li>
+            <li>1 hour or shorter walks, tailored to your needs</li>
+            <li>Friendly, professional, personal service</li>
+            <li>Canine First Aid Certified</li>
+            <li>Fully insured and DBS Checked</li>
+        </ul>
     </div>
   );
 }
