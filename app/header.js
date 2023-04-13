0a8390638b743f96caf216eb65e1be8e0ca7d56c
James Donald <dev@chaoscantrip.com>
2023-04-13 04:09:05 +0100

    Added basic header

--- app/header.js
+++ app/header.js
@@ -1,5 +1,10 @@
+import styles from "@/styles/modules/header.module.css";
+
 export default function Header(){
     return (
-        <header></header>
+        <header className={styles.header}>
+            <h1>Happy Trails Waggy Tails</h1>
+            <p>Website currently under development :)</p>
+        </header>
     )
 }
