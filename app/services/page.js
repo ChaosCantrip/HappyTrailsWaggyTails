315e04ae023ef3aa68efbd129f29906d099e2221
James Donald <dev@chaoscantrip.com>
2023-04-13 04:35:56 +0100

    Added about, contact, services pages

---
+++ app/services/page.js
@@ -0,0 +1,10 @@
+export default function ServicesPage() {
+    return (
+        <div>
+            <h1>Services</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A amet, architecto aspernatur atque blanditiis
+                dicta doloribus earum excepturi exercitationem labore maxime omnis quisquam, quod sint soluta sunt ullam
+                velit voluptatum.</p>
+        </div>
+    )
+}
