d42102793b44c74e6542abd8365c906c2b902b67
James Donald <dev@chaoscantrip.com>
2023-04-13 04:02:34 +0100

    Added header and footer

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,6 @@
 import "@/styles/global/master.css";
+import Header from "@/app/header";
+import Footer from "@/app/footer";
 
 export const metadata = {
     title: {
@@ -10,7 +12,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
  return (
     <html lang="en">
-      <body>{children}</body>
+        <body>
+            <Header />
+            <main>
+                {children}
+            </main>
+            <Footer />
+        </body>
     </html>
   )
 }
