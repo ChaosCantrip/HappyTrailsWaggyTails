dcc7425e8904fa796118246574617abe1dad57d2
ChaosCantrip <dev@chaoscantrip.com>
2023-04-14 16:02:54 +0100

    Changed About to grid layout

--- app/about/page.js
+++ app/about/page.js
@@ -1,35 +1,39 @@
+import layout from "@/styles/modules/layout.module.css";
+
 export const metadata = {
         title: "About"
 }
 
 export default function AboutPage() {
     return (
-        <div>
-            <h1>About Me</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A amet, architecto aspernatur atque blanditiis
-                dicta doloribus earum excepturi exercitationem labore maxime omnis quisquam, quod sint soluta sunt ullam
-                velit voluptatum.</p>
-            <section>
-                <p>Content</p>
-                <p>To</p>
-                <p>Push</p>
-                <p>Footer</p>
-                <p>Down</p>
-            </section>
-            <section>
-                <p>Content</p>
-                <p>To</p>
-                <p>Push</p>
-                <p>Footer</p>
-                <p>Down</p>
-            </section>
-            <section>
-                <p>Content</p>
-                <p>To</p>
-                <p>Push</p>
-                <p>Footer</p>
-                <p>Down</p>
-            </section>
+        <div className={layout.flexbox}>
+            <div>
+                <h1>About Me</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A amet, architecto aspernatur atque blanditiis
+                    dicta doloribus earum excepturi exercitationem labore maxime omnis quisquam, quod sint soluta sunt ullam
+                    velit voluptatum.</p>
+                <section>
+                    <p>Content</p>
+                    <p>To</p>
+                    <p>Push</p>
+                    <p>Footer</p>
+                    <p>Down</p>
+                </section>
+                <section>
+                    <p>Content</p>
+                    <p>To</p>
+                    <p>Push</p>
+                    <p>Footer</p>
+                    <p>Down</p>
+                </section>
+                <section>
+                    <p>Content</p>
+                    <p>To</p>
+                    <p>Push</p>
+                    <p>Footer</p>
+                    <p>Down</p>
+                </section>
+            </div>
         </div>
     )
 }
