7903ec68de004039a55822ec77e3602d0ba19395
ChaosCantrip <dev@chaoscantrip.com>
2023-04-15 20:56:39 +0100

    Changed layout to grid

--- app/services/page.js
+++ app/services/page.js
@@ -1,14 +1,18 @@
+import layout from "@/styles/modules/layout.module.css";
+
 export const metadata = {
     title: "Services"
 }
 
 export default function ServicesPage() {
     return (
-        <div>
-            <h1>Services</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A amet, architecto aspernatur atque blanditiis
-                dicta doloribus earum excepturi exercitationem labore maxime omnis quisquam, quod sint soluta sunt ullam
-                velit voluptatum.</p>
+        <div className={layout.page}>
+            <div className={layout.double}>
+                <h1>Services</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A amet, architecto aspernatur atque blanditiis
+                    dicta doloribus earum excepturi exercitationem labore maxime omnis quisquam, quod sint soluta sunt ullam
+                    velit voluptatum.</p>
+            </div>
         </div>
     )
 }
