b2e676668b6e6fc48023290c27513789012ad352
ChaosCantrip <dev@chaoscantrip.com>
2023-04-16 03:55:47 +0100

    Added underline to navbar

--- app/header.js
+++ app/header.js
@@ -1,10 +1,24 @@
+"use client";
+
 import Link from "next/link";
 import logo from "@/public/images/logo_no_text.png";
 
 import styles from "@/styles/modules/header.module.css";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
+import {pathHasPrefix} from "next/dist/shared/lib/router/utils/path-has-prefix";
 
 export default function Header(){
+    function get_class(path) {
+        console.log(pathname);
+        console.log(path);
+        console.log(pathname === path);
+        if (pathname === path) {
+            return styles.active;
+        }
+        return styles.inactive;
+    }
+    const pathname = usePathname();
     return (
         <header className={styles.wrapper}>
             <div className={styles.content}>
@@ -17,10 +31,10 @@ export default function Header(){
                 </div>
                 <nav className={styles.nav}>
                     <ul>
-                        <Link href="home"><li>Home</li></Link>
-                        <Link href="about"><li>About</li></Link>
-                        <Link href="services"><li>Services</li></Link>
-                        <Link href="contact"><li>Contact</li></Link>
+                        <Link href="home" className={get_class("/home")}><li>Home</li></Link>
+                        <Link href="about" className={get_class("/about")}><li>About</li></Link>
+                        <Link href="services" className={get_class("/services")}><li>Services</li></Link>
+                        <Link href="contact" className={get_class("/contact")}><li>Contact</li></Link>
                     </ul>
                 </nav>
             </div>
