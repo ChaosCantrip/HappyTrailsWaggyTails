1e8384cd5eaeea33f35ae3e711cf492b120a528e
ChaosCantrip <dev@chaoscantrip.com>
2023-04-23 07:38:42 +0100

    Added lib/email -> send_email()

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -1,25 +1,14 @@
-import nodemailer from 'nodemailer';
+import { send_email } from '@/lib/email';
 import { NextResponse } from 'next/server';
 
 export async function POST(request) {
     try {
         const data = await request.json();
-        const transporter = nodemailer.createTransport({
-            host: process.env.SMTP_HOST,
-            port: process.env.SMTP_PORT,
-            secure: true,
-            auth: {
-                user: process.env.SMTP_USER,
-                pass: process.env.SMTP_PASS
-            }
-        });
-        const mailOptions = {
-            from: process.env.SMTP_MAIL,
+        await send_email({
             to: process.env.SMTP_MAIL,
             subject: 'Contact Form',
             text: `Name: ${data.name}\nPhone: ${data.phone}\nEmail: ${data.email}\nMessage: ${data.message}`
-        };
-        await transporter.sendMail(mailOptions);
+        })
         return NextResponse.json("Success", { status: 200 })
     } catch (e) {
         console.log(e);
