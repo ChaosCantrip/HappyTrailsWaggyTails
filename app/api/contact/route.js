c40ba0f7427a1bb3bb2a18aecb689d039e0f81e6
ChaosCantrip <dev@chaoscantrip.com>
2023-04-23 18:15:20 +0100

    Added basic html emails

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -1,5 +1,7 @@
-import { send_email } from '@/lib/email';
+import { send_email, format_email } from '@/lib/email';
 import { NextResponse } from 'next/server';
+import { promises as fs } from 'fs';
+import path from 'path';
 
 export async function POST(request) {
     try {
@@ -7,12 +9,14 @@ export async function POST(request) {
         await send_email({
             to: process.env.SMTP_MAIL,
             subject: 'Contact Form',
-            text: `Name: ${data.name}\nPhone: ${data.phone}\nEmail: ${data.email}\nMessage: ${data.message}`
+            text: `Name: ${data.name}\nPhone: ${data.phone}\nEmail: ${data.email}\nMessage: ${data.message}`,
+            html: await format_email("ContactForm", data)
         });
         await send_email({
             to: data.email,
             subject: "Happy Trails Waggy Tails",
-            text: "Thank you for contacting Happy Trails Waggy Tails. I'll be in touch soon!"
+            text: "Thank you for contacting Happy Trails Waggy Tails. I'll be in touch soon!",
+            html: await format_email("ThankYou", data)
         });
         return NextResponse.json("Success", { status: 200 })
     } catch (e) {
