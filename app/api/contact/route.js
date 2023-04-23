b0356db90b619968be68287937b7b54786073294
ChaosCantrip <dev@chaoscantrip.com>
2023-04-23 05:59:06 +0100

    Added temporary error test

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -4,5 +4,8 @@ export async function POST(request) {
     console.log("Hit on /api/contact");
     const data = await request.json();
     console.log(data);
+    if (data["message"].toLowerCase() === "error") {
+        return NextResponse.json("Error", { status: 500 })
+    }
     return NextResponse.json('Thanks for contacting us, we will get back to you soon!');
 }
