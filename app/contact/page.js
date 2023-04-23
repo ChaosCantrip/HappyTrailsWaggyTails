a5a3cc5cbf18a0a9c2b500d6371c5a3433614a9a
ChaosCantrip <dev@chaoscantrip.com>
2023-04-23 04:57:29 +0100

    Updated Contact

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,4 +1,6 @@
 import layout from "@/styles/modules/layout.module.css";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faEnvelope, faPhone} from "@fortawesome/free-solid-svg-icons";
 
 export const metadata = {
     title: "Contact"
@@ -9,9 +11,15 @@ export default function ContactPage() {
         <div className={layout.page}>
             <div className={layout.double}>
                 <h1>Contact Me</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A amet, architecto aspernatur atque blanditiis
-                    dicta doloribus earum excepturi exercitationem labore maxime omnis quisquam, quod sint soluta sunt ullam
-                    velit voluptatum.</p>
+            </div>
+            <div className={layout.left}>
+                <p>To enquire about my services, please contact me using the details below.</p>
+                <p><FontAwesomeIcon icon={faPhone}/> Phone: <a href="tel:07425 160615">07425 160615</a></p>
+                <p><FontAwesomeIcon icon={faEnvelope}/> Email: <a href="mailto:nadine@happytrailswaggytails.co.uk">nadine@happytrailswaggytails.co.uk</a></p>
+            </div>
+            <div className={layout.right}>
+                <h1>I'm on Facebook!</h1>
+                <p>Embed here innit</p>
             </div>
         </div>
     )
