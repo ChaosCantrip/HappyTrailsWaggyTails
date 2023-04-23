8699c41adeb051650b3c8ad443c7baa9384063a6
ChaosCantrip <dev@chaoscantrip.com>
2023-04-23 07:40:14 +0100

    Added thank you email

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -8,7 +8,12 @@ export async function POST(request) {
             to: process.env.SMTP_MAIL,
             subject: 'Contact Form',
             text: `Name: ${data.name}\nPhone: ${data.phone}\nEmail: ${data.email}\nMessage: ${data.message}`
-        })
+        });
+        await send_email({
+            to: data.email,
+            subject: "Happy Trails Waggy Tails",
+            text: "Thank you for contacting Happy Trails Waggy Tails. I'll be in touch soon!"
+        });
         return NextResponse.json("Success", { status: 200 })
     } catch (e) {
         console.log(e);
