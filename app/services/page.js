63e86316c50f03f52a3f36d7452ba39c9f59ae9f
ChaosCantrip <dev@chaoscantrip.com>
2023-04-23 04:57:22 +0100

    Updated Services

--- app/services/page.js
+++ app/services/page.js
@@ -1,4 +1,7 @@
 import layout from "@/styles/modules/layout.module.css";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faPaw} from "@fortawesome/free-solid-svg-icons";
+import styles from "@/styles/modules/home.module.css";
 
 export const metadata = {
     title: "Services"
@@ -9,10 +12,27 @@ export default function ServicesPage() {
         <div className={layout.page}>
             <div className={layout.double}>
                 <h1>Services</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A amet, architecto aspernatur atque blanditiis
-                    dicta doloribus earum excepturi exercitationem labore maxime omnis quisquam, quod sint soluta sunt ullam
-                    velit voluptatum.</p>
+                <br/>
+                <ul className={styles.list}>
+                    <li>Free consultation to determine your exact needs</li>
+                    <li><PawIcon/></li>
+                    <li>Solo walks for dogs who are a little nervous</li>
+                    <li><PawIcon/></li>
+                    <li>Group walks for dogs who love to socialise</li>
+                    <li><PawIcon/></li>
+                    <li>30 minute walks for dogs who need a little exercise</li>
+                    <li><PawIcon/></li>
+                    <li>1 hour walks for dogs to let out their energy</li>
+                    <li><PawIcon/></li>
+                    <li>Home visits for toilet breaks</li>
+                </ul>
             </div>
         </div>
+    )
+}
+
+function PawIcon(){
+    return (
+        <FontAwesomeIcon icon={faPaw} className={styles.paw}/>
     )
 }
