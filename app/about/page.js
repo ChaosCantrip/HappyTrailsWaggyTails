f6edc52de45180ab45b3ae3ee4047dd6545a1f93
ChaosCantrip <dev@chaoscantrip.com>
2023-04-23 04:44:51 +0100

    Added About Me Contents

--- app/about/page.js
+++ app/about/page.js
@@ -9,31 +9,14 @@ export default function AboutPage() {
     return (
         <div className={layout.page}>
             <div>
-                <h1>About Me</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A amet, architecto aspernatur atque blanditiis
-                    dicta doloribus earum excepturi exercitationem labore maxime omnis quisquam, quod sint soluta sunt ullam
-                    velit voluptatum.</p>
-                <section>
-                    <p>Content</p>
-                    <p>To</p>
-                    <p>Push</p>
-                    <p>Footer</p>
-                    <p>Down</p>
-                </section>
-                <section>
-                    <p>Content</p>
-                    <p>To</p>
-                    <p>Push</p>
-                    <p>Footer</p>
-                    <p>Down</p>
-                </section>
-                <section>
-                    <p>Content</p>
-                    <p>To</p>
-                    <p>Push</p>
-                    <p>Footer</p>
-                    <p>Down</p>
-                </section>
+                <h1>Hi, I'm Nadine!</h1>
+                <br/>
+                <p>For the last 9 years, I have been working with children who have Special Educational Needs,
+                    which has been very rewarding.</p>
+                <p>However, after saying goodbye to our beloved Monty, and him leaving a huge hole in our hearts and
+                    home, I feel now is the time to follow a long held dream of mine and work with
+                    dogs, so I have started Happy Trails Waggy Tails dog walking service.</p>
+                <p>I would love to hear from you and meet your doggy member of the family</p>
             </div>
             <div>
                 <MontyGallery/>
