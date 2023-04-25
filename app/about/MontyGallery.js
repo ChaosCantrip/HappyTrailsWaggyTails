ae2d2ba88e46b1419e0b810d2753005f4300e4ab
ChaosCantrip <dev@chaoscantrip.com>
2023-04-25 09:49:43 +0100

    Updated gallery imports

--- app/about/MontyGallery.js
+++ app/about/MontyGallery.js
@@ -2,24 +2,24 @@
 
 import styles from "@/styles/modules/gallery.module.css";
 
-import Photo1 from "@/public/images/monty/monty1.jpg";
-import Photo2 from "@/public/images/monty/monty2.jpg";
-import Photo3 from "@/public/images/monty/monty3.jpg";
-import Photo4 from "@/public/images/monty/monty4.jpg";
-import Photo5 from "@/public/images/monty/monty5.jpg";
-import Photo6 from "@/public/images/monty/monty6.jpg";
-import Photo7 from "@/public/images/monty/monty7.jpg";
-import Photo8 from "@/public/images/monty/monty8.jpg";
-import Photo9 from "@/public/images/monty/monty9.jpg";
-import Photo10 from "@/public/images/monty/monty10.jpg";
-import Photo11 from "@/public/images/monty/monty11.jpg";
-import Photo12 from "@/public/images/monty/monty12.jpg";
-import Photo13 from "@/public/images/monty/monty13.jpg";
-import Photo14 from "@/public/images/monty/monty14.jpg";
-import Photo15 from "@/public/images/monty/monty15.jpg";
-import Photo16 from "@/public/images/monty/monty16.jpg";
-import Photo17 from "@/public/images/monty/monty17.jpg";
-import Photo18 from "@/public/images/monty/monty18.jpg";
+import Photo1 from "@/public/images/monty/full/monty1.jpg";
+import Photo2 from "@/public/images/monty/full/monty2.jpg";
+import Photo3 from "@/public/images/monty/full/monty3.jpg";
+import Photo4 from "@/public/images/monty/full/monty4.jpg";
+import Photo5 from "@/public/images/monty/full/monty5.jpg";
+import Photo6 from "@/public/images/monty/full/monty6.jpg";
+import Photo7 from "@/public/images/monty/full/monty7.jpg";
+import Photo8 from "@/public/images/monty/full/monty8.jpg";
+import Photo9 from "@/public/images/monty/full/monty9.jpg";
+import Photo10 from "@/public/images/monty/full/monty10.jpg";
+import Photo11 from "@/public/images/monty/full/monty11.jpg";
+import Photo12 from "@/public/images/monty/full/monty12.jpg";
+import Photo13 from "@/public/images/monty/full/monty13.jpg";
+import Photo14 from "@/public/images/monty/full/monty14.jpg";
+import Photo15 from "@/public/images/monty/full/monty15.jpg";
+import Photo16 from "@/public/images/monty/full/monty16.jpg";
+import Photo17 from "@/public/images/monty/full/monty17.jpg";
+import Photo18 from "@/public/images/monty/full/monty18.jpg";
 
 import Image from "next/image";
 import { useEffect } from "react";
@@ -80,24 +80,24 @@ export default function MontyGallery() {
     return (
         <div className={styles.wrapper}>
             <div id="gallery" className={styles.gallery}>
-                <Image id="img1" onClick={() => handleClick("img1")} src={Photo1} alt={"1"} className={`${styles.x0} ${styles.y0}`}/>
-                <Image id="img2" onClick={() => handleClick("img2")} src={Photo2} alt={"2"} className={`${styles.x1} ${styles.y0}`}/>
-                <Image id="img3" onClick={() => handleClick("img3")} src={Photo3} alt={"3"} className={`${styles.x2} ${styles.y0}`}/>
-                <Image id="img4" onClick={() => handleClick("img4")} src={Photo4} alt={"4"} className={`${styles.x0} ${styles.y1}`}/>
-                <Image id="img5" onClick={() => handleClick("img5")} src={Photo5} alt={"5"} className={`${styles.x1} ${styles.y1}`}/>
-                <Image id="img6" onClick={() => handleClick("img6")} src={Photo6} alt={"6"} className={`${styles.x2} ${styles.y1}`}/>
-                <Image id="img7" onClick={() => handleClick("img7")} src={Photo7} alt={"7"} className={`${styles.x0} ${styles.y2}`}/>
-                <Image id="img8" onClick={() => handleClick("img8")} src={Photo8} alt={"8"} className={`${styles.x1} ${styles.y2}`}/>
-                <Image id="img9" onClick={() => handleClick("img9")} src={Photo9} alt={"9"} className={`${styles.x2} ${styles.y2}`}/>
-                <Image id="img10" onClick={() => handleClick("img10")} src={Photo10} alt={"10"} className={`${styles.x0} ${styles.y0} ${styles.hidden}`}/>
-                <Image id="img11" onClick={() => handleClick("img11")} src={Photo11} alt={"11"} className={`${styles.x0} ${styles.y0} ${styles.hidden}`}/>
-                <Image id="img12" onClick={() => handleClick("img12")} src={Photo12} alt={"12"} className={`${styles.x0} ${styles.y0} ${styles.hidden}`}/>
-                <Image id="img13" onClick={() => handleClick("img13")} src={Photo13} alt={"13"} className={`${styles.x0} ${styles.y0} ${styles.hidden}`}/>
-                <Image id="img14" onClick={() => handleClick("img14")} src={Photo14} alt={"14"} className={`${styles.x0} ${styles.y0} ${styles.hidden}`}/>
-                <Image id="img15" onClick={() => handleClick("img15")} src={Photo15} alt={"15"} className={`${styles.x0} ${styles.y0} ${styles.hidden}`}/>
-                <Image id="img16" onClick={() => handleClick("img16")} src={Photo16} alt={"16"} className={`${styles.x0} ${styles.y0} ${styles.hidden}`}/>
-                <Image id="img17" onClick={() => handleClick("img17")} src={Photo17} alt={"17"} className={`${styles.x0} ${styles.y0} ${styles.hidden}`}/>
-                <Image id="img18" onClick={() => handleClick("img18")} src={Photo18} alt={"18"} className={`${styles.x0} ${styles.y0} ${styles.hidden}`}/>
+                <Image id="img1" onClick={() => handleClick("img1")} src={Photo1} blurDataURL={"@/public/images/monty/blurred/monty1.jpg"} placeholder="blur" alt={"1"} className={`${styles.x0} ${styles.y0}`}/>
+                <Image id="img2" onClick={() => handleClick("img2")} src={Photo2} blurDataURL={"@/public/images/monty/blurred/monty2.jpg"} placeholder="blur" alt={"2"} className={`${styles.x1} ${styles.y0}`}/>
+                <Image id="img3" onClick={() => handleClick("img3")} src={Photo3} blurDataURL={"@/public/images/monty/blurred/monty3.jpg"} placeholder="blur" alt={"3"} className={`${styles.x2} ${styles.y0}`}/>
+                <Image id="img4" onClick={() => handleClick("img4")} src={Photo4} blurDataURL={"@/public/images/monty/blurred/monty4.jpg"} placeholder="blur" alt={"4"} className={`${styles.x0} ${styles.y1}`}/>
+                <Image id="img5" onClick={() => handleClick("img5")} src={Photo5} blurDataURL={"@/public/images/monty/blurred/monty5.jpg"} placeholder="blur" alt={"5"} className={`${styles.x1} ${styles.y1}`}/>
+                <Image id="img6" onClick={() => handleClick("img6")} src={Photo6} blurDataURL={"@/public/images/monty/blurred/monty6.jpg"} placeholder="blur" alt={"6"} className={`${styles.x2} ${styles.y1}`}/>
+                <Image id="img7" onClick={() => handleClick("img7")} src={Photo7} blurDataURL={"@/public/images/monty/blurred/monty7.jpg"} placeholder="blur" alt={"7"} className={`${styles.x0} ${styles.y2}`}/>
+                <Image id="img8" onClick={() => handleClick("img8")} src={Photo8} blurDataURL={"@/public/images/monty/blurred/monty8.jpg"} placeholder="blur" alt={"8"} className={`${styles.x1} ${styles.y2}`}/>
+                <Image id="img9" onClick={() => handleClick("img9")} src={Photo9} blurDataURL={"@/public/images/monty/blurred/monty9.jpg"} placeholder="blur" alt={"9"} className={`${styles.x2} ${styles.y2}`}/>
+                <Image id="img10" onClick={() => handleClick("img10")} src={Photo10} blurDataURL={"@/public/images/monty/blurred/monty10.jpg"} placeholder="blur" alt={"10"} className={`${styles.x0} ${styles.y0} ${styles.hidden}`}/>
+                <Image id="img11" onClick={() => handleClick("img11")} src={Photo11} blurDataURL={"@/public/images/monty/blurred/monty11.jpg"} placeholder="blur" alt={"11"} className={`${styles.x0} ${styles.y0} ${styles.hidden}`}/>
+                <Image id="img12" onClick={() => handleClick("img12")} src={Photo12} blurDataURL={"@/public/images/monty/blurred/monty12.jpg"} placeholder="blur" alt={"12"} className={`${styles.x0} ${styles.y0} ${styles.hidden}`}/>
+                <Image id="img13" onClick={() => handleClick("img13")} src={Photo13} blurDataURL={"@/public/images/monty/blurred/monty13.jpg"} placeholder="blur" alt={"13"} className={`${styles.x0} ${styles.y0} ${styles.hidden}`}/>
+                <Image id="img14" onClick={() => handleClick("img14")} src={Photo14} blurDataURL={"@/public/images/monty/blurred/monty14.jpg"} placeholder="blur" alt={"14"} className={`${styles.x0} ${styles.y0} ${styles.hidden}`}/>
+                <Image id="img15" onClick={() => handleClick("img15")} src={Photo15} blurDataURL={"@/public/images/monty/blurred/monty15.jpg"} placeholder="blur" alt={"15"} className={`${styles.x0} ${styles.y0} ${styles.hidden}`}/>
+                <Image id="img16" onClick={() => handleClick("img16")} src={Photo16} blurDataURL={"@/public/images/monty/blurred/monty16.jpg"} placeholder="blur" alt={"16"} className={`${styles.x0} ${styles.y0} ${styles.hidden}`}/>
+                <Image id="img17" onClick={() => handleClick("img17")} src={Photo17} blurDataURL={"@/public/images/monty/blurred/monty17.jpg"} placeholder="blur" alt={"17"} className={`${styles.x0} ${styles.y0} ${styles.hidden}`}/>
+                <Image id="img18" onClick={() => handleClick("img18")} src={Photo18} blurDataURL={"@/public/images/monty/blurred/monty18.jpg"} placeholder="blur" alt={"18"} className={`${styles.x0} ${styles.y0} ${styles.hidden}`}/>
             </div>
         </div>
     )
