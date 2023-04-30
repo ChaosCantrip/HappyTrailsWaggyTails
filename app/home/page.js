1d282cf8cf9037627542c3b5525ce046f14de0ac
ChaosCantrip <dev@chaoscantrip.com>
2023-04-30 16:37:25 +0100

    Amended pages

--- app/home/page.js
+++ app/home/page.js
@@ -1,5 +1,3 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPaw } from "@fortawesome/free-solid-svg-icons";
 import logo from "@/public/images/logo.jpg";
 
 export const metadata = {
@@ -17,23 +15,14 @@ export default function HomePage() {
             <div className={styles.intro}>
                 <h1>Hi, I'm Nadine!</h1>
                 <br/>
-                <p>For the last 9 years, I have been working with children who have Special Educational Needs,
-                    which has been very rewarding.</p>
-                <p>However, after saying goodbye to our beloved Monty, and him leaving a huge hole in our hearts and
-                    home, I feel now is the time to follow a long held dream of mine and work with
-                    dogs, so I have started Happy Trails Waggy Tails dog walking service.</p>
-                <p>I would love to hear from you and meet your doggy member of the family</p>
+                <p>Welcome to Happy Tails Waggy Tails Dog Walking Service.</p>
+                <p>Areas covered are Barnby Dun, Kirk Sandall, Edenthorpe, Hatfield, Dunsville and Dunscroft. Other areas will be considered.</p>
+                <p>I look forward to meeting you and your doggy member of the family!</p>
             </div>
         </div>
         <div className={`${layout.desktop_only}`}>
-            <Image src={logo} alt="Logo" className={styles.logo}/>
+            <Image src={logo} alt="Logo" className={styles.logo} placeholder="blur"/>
         </div>
     </div>
   );
-}
-
-function PawIcon(){
-    return (
-        <FontAwesomeIcon icon={faPaw} className={styles.paw}/>
-    )
 }
