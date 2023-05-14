9b8c6bf0c35b81c912386117301f306493110f11
ChaosCantrip <dev@chaoscantrip.com>
2023-05-14 16:16:28 +0100

    Made redirects less cursed

---
+++ app/(home)/page.js
@@ -0,0 +1,28 @@
+import logo from "@/public/images/logo.jpg";
+
+export const metadata = {
+    title: "Home"
+}
+
+import styles from "@/styles/modules/home.module.css";
+import layout from "@/styles/modules/layout.module.css";
+import Image from "next/image";
+
+export default function HomePage() {
+  return (
+    <div className={layout.page}>
+        <div>
+            <div className={styles.intro}>
+                <h1>Hi, I'm Nadine!</h1>
+                <br/>
+                <p>Welcome to Happy Tails Waggy Tails Dog Walking Service.</p>
+                <p>Areas covered are Barnby Dun, Kirk Sandall, Edenthorpe, Hatfield, Dunsville and Dunscroft. Other areas near Doncaster will be considered.</p>
+                <p>I look forward to meeting you and your doggy member of the family!</p>
+            </div>
+        </div>
+        <div className={`${layout.desktop_only}`}>
+            <Image src={logo} alt="Logo" className={styles.logo} placeholder="blur"/>
+        </div>
+    </div>
+  );
+}
